c6bc2b2265e4552e3ac2017dffa639262307215c
kaRaGODDD <98781408+kaRaGODDD@users.noreply.github.com>
2023-07-04 19:44:32 +0300

    Create DubkovFirstASMLAB_var8.cpp

---
+++ Third Semester/ASM_LABS/DubkovFirstASMLAB_var8.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+/*
+From the record of a natural number N, discard all digits that are multiples of the given one, leaving the order of the remaining digits the same.
+*/
+int main() {
+
+	int a,
+		k,
+		sum{},
+		i = 1,
+		ten = 10;
+
+	std::cin >> a >> k;
+
+	__asm {
+		xor eax,eax
+		xor ecx,ecx
+		mov eax,a
+	_start:
+			xor edx, edx
+			cmp eax, 0
+			je _nothing
+
+			div ten
+			mov ecx, eax
+			mov eax, edx
+			xor edx,edx
+			div k
+			cmp edx,0
+			jne _yeah 
+			je _yep
+	_yeah:
+		mov esi,edx
+		xor edx,edx
+		mul k
+		add eax,esi
+		xor esi,esi
+		mul i //123456789  
+		add sum,eax
+		xor eax, eax
+		cmp ecx,0
+		je L1
+		mov eax, i
+		mul ten
+		mov i,eax
+		mov eax,ecx
+		xor ecx,ecx		
+		L1 :
+		jmp _start
+	_yep:
+		mov eax,ecx
+		xor ecx,ecx
+		jmp _start
+			
+	jmp _start
+	_nothing:
+		nop
+			
+	}
+	std::cout << sum << "\n";
+	return EXIT_SUCCESS;
+}
